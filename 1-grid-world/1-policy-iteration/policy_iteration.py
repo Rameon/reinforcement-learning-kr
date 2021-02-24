6ea88d71af65e049b79ae65054ef1a91eb7ea2e1
Rameon <Yirameon@gmail.com>
2021-02-24 15:58:58 +0900

    Update policy_iteration.py
    "그리드월드에서 에이전트가 알고 있는 환경의 정보"에 대해 주석 설명을 추가함

--- 1-grid-world/1-policy-iteration/policy_iteration.py
+++ 1-grid-world/1-policy-iteration/policy_iteration.py
@@ -1,12 +1,36 @@
 # -*- coding: utf-8 -*-
 import random
+
+# environment.py 안에 있는 GraphicDisplay, Env 클래스를 import함
+# GraphicDisplay 는 GUI로 그리드월드 환경을 보여주는 클래스임
 from environment import GraphicDisplay, Env
 
 
+# 정책 이터레이션의 에이전트는 PolicyIteration 클래스로 정의되어 있음
 class PolicyIteration:
-    def __init__(self, env):
+    def __init__(self, env):  # PolicyIteration 클래스의 정의에서 env를 self.env로서 정의함
         # 환경에 대한 객체 선언
+        # 에이전트에게는 환경에 대한 정보가 필요하므로 main 루프에서 Env()를 env 객체로 생성함
+        # 이 env 객체를 PolicyIteration 클래스의 인수로 전달함으로써, 에이전트는 환경의 Env() 클래스에 접근할 수 있음
         self.env = env
+
+        # < env 객체에 정의돼 있는 변수와 함수 >
+        # env.width, env.height : 그리드 월드의 너비, 높이 -> 반환값 : 그리드월드의 가로, 세로를 정수로 반환함
+
+        # env.state_after_action(state, action) : 에이전트가 특정 상태에서 특정 행동을 했을 때, 에이전트가 가는 다음 상태
+        #                                         -> 반환값 : 행동 후의 상태를 좌표로 표현한 리스트를 반환함 예) [1,2]
+
+        # DP에서는 에이전트가 모든 상태에 대해 벨만 방정식을 계산하는데, 따라서 에이전트는 가능한 모든 상태를 알아야 함
+        # 이 모든 상태들은 env.get_all_state() 를 통해 할 수 있음
+
+        # env.get_all_state() : 존재하는 모든 상태 -> 반환값 : 모든 상태를 반환함 예) [[0,0], [0,1], ... , [4,4]]
+
+        # env.get_reward(state, action) : 특정 상태의 보상(환경이 주는 보상) -> 반환값 : 정수의 형태로 보상을 반환함
+
+        # env.possible_actions : 상, 하, 좌, 우(에이전트의 가능한 모든 행동) -> 반환값 : [0,1,2,3]을 반환함, 순서대로 상,하,좌,우를 의미함
+
+        # 보상과 상태 변환 확률은 에이전트가 아니라, 환경에 속한 것이므로 env 객체로 정의함
+
         # 가치함수를 2차원 리스트로 초기화
         self.value_table = [[0.0] * env.width for _ in range(env.height)]
         # 상 하 좌 우 동일한 확률로 정책 초기화
@@ -102,6 +126,7 @@ class PolicyIteration:
     def get_value(self, state):
         # 소숫점 둘째 자리까지만 계산
         return round(self.value_table[state[0]][state[1]], 2)
+
 
 if __name__ == "__main__":
     env = Env()
